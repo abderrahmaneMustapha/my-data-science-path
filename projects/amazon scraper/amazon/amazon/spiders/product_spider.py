755fe952e85006a8b0c0ae940b5a5e208f12670d
abderrahmaneMustapha <abderrahmanemustapha030898@gmail.com>
2020-05-21 06:00:37 +0100

    get projects titles

--- projects/amazon scraper/amazon/amazon/spiders/product_spider.py
+++ projects/amazon scraper/amazon/amazon/spiders/product_spider.py
@@ -1,12 +1,12 @@
 import scrapy
 
 
-class QuotesSpider(scrapy.Spider):
-    name = "amazon-main"
+class AmazonSpider(scrapy.Spider):
+    name = "amazon"
 
     def start_requests(self):
         urls = [
-            'https://www.amazon.com/?ref=icp_country_in_t1',
+            'https://www.amazon.com/b?node=17938598011&pf_rd_r=W8409C2R445HHWYXHT7A&pf_rd_p=e5b0c85f-569c-4c90-a58f-0c0a260e45a0',
         ]
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
@@ -15,5 +15,6 @@ class QuotesSpider(scrapy.Spider):
         page = response.url.split("/")[-2]
         filename = 'text-%s.html' % page
         with open(filename, 'wb') as f:
-            f.write(response.body)
+            for r in  response.css(u"div li.s-result-item a.s-access-detail-page h2").getall():
+                f.write(r.encode())
         self.log('Saved file %s' % filename)
